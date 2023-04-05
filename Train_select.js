e7731627319f1e76bb9e7e3d9f0f6b65f1403491
AHN DONGMIN <dmahn@tecosys.co.jp>
2023-04-05 18:51:59 +0900

    04-05

---
+++ Train_select.js
@@ -0,0 +1,11 @@
+let url = window.location.search;
+let link = new URLSearchParams(url);
+var year = link.get("year");
+var month = link.get("month");
+var day = link.get("day");
+var inputHour = link.get("inputHour");
+var inputMinute = link.get("inputMinute");
+var inputTransportType = link.get("inputTransportType");
+var inputDepartStName = link.get("inputDepartStName");
+var inputArriveStName = link.get("inputArriveStName");
+
