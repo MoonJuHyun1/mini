08dce3ce64618107eec97b9bc2e45dd19ee87099
caesar9132 <129859262+caesar9132@users.noreply.github.com>
2023-04-06 15:10:31 +0900

    Add files via upload

---
+++ sample1.js
@@ -0,0 +1,32 @@
+    ej.base.enableRipple(true);
+
+    var data = new ej.data.DataManager(window.hierarchyOrderdata).executeLocal(new ej.data.Query().take(15));
+    var grid = new ej.grids.Grid({
+    dataSource: data,
+    columns: [
+                { field: '列車番号', headerText: '列車番', width: 120, textAlign: 'Right' },
+                { field: '乗車日',  headerText: '乗車', width: 150, textAlign: 'Right' },
+                { field: '出発/到着t',headerText :出発/到着, width: 120, format: 'C2', textAlign: 'Right' },
+                { field: '料金', headerText: '料金', width: 150 }
+            ]
+           
+            
+    });
+    grid.appendTo('#Grid');
+
+    var button = new ej.buttons.Button({ cssClass: `e-primary` }, '#close');
+    var submitbutton = new ej.buttons.Button({ cssClass: `e-primary` }, '#submit');
+
+    document.getElementById("Gridform").addEventListener("A1", (e) => {
+    e.preventDefault();
+    var value = parseInt(document.getElementById('multiplier').value, 10);
+    data = new ej.data.DataManager(window.hierarchyOrderdata).executeLocal(new ej.data.Query().where("EmployeeID", "equal", value).take(15))
+    grid.dataSource = data;
+    document.getElementById("userinput").style.display = "none";
+    document.getElementById("mtable").style.display = "";
+    document.getElementById("Gridform").reset();
+    });
+    document.getElementById("close").addEventListener("click", (e)=>{
+    document.getElementById("mtable").style.display = "none";
+    document.getElementById("userinput").style.display = "";
+    });
