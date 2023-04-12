bab1162fe5ab27256df4f332db9eaffb44ea2873
moonjuhyun1 <jhmoon@tecosys.co.jp>
2023-04-12 15:21:41 +0900

    "04-12"

---
+++ test1.c
@@ -0,0 +1,14 @@
+//int포인터 변수와 배열의 개수를 인수로 받아 해당 포인터의 내용을 복사하는 함수를 작성.
+
+// method(int cnt, int*src, int* dest)
+// {
+//     printf("ab");
+// }
+
+int a = 1;
+printf(a);
+
+// void printNo1(int* p_dest, int* p_src,int size)
+// {
+//     for()
+// }
