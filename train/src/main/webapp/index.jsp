c71b56d5dcbcd9e8e71862a0181a3d7acc73ffc9
eunjinlee94 <tmmt94@gmail.com>
2023-04-20 22:33:31 +0900

    springproject_upload

--- train/src/main/webapp/index.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>test</title>
-  </head>
-  <body>
-  <h3>test~</h3>
-  </body>
-</html>
---
+++ train/train/src/main/java/com/teco/train/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.teco.train.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
